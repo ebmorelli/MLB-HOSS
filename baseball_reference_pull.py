8f56a6313bc751c27a3fa2063e013147781cd0e4
Eddie Morelli <edmo2495@gmail.com>
2025-08-15 21:33:48 -0500

    separate script for br pull, calculate ht and wt percentile instead of z-score

---
+++ baseball_reference_pull.py
@@ -0,0 +1,27 @@
+# Pull all pitchers' height and weight from Baseball Reference
+# Note: need to use vpn to avoid HTTP 429 error
+
+import pandas as pd
+import time
+
+teams = [
+    "ARI", "ATL", "BAL", "BOS", "CHC", "CHW", "CIN", "CLE",
+    "COL", "DET", "HOU", "KCR", "LAA", "LAD", "MIA", "MIL",
+    "MIN", "NYM", "NYY", "OAK", "PHI", "PIT", "SDP", "SEA",
+    "SFG", "STL", "TBR", "TEX", "TOR", "WSN"
+]
+year = 2025
+
+pitchers = None
+
+for team in teams:
+    url = f"https://www.baseball-reference.com/teams/{team}/{year}-roster.shtml"
+    tables = pd.read_html(url)
+    roster = tables[0]
+    team_pitchers = roster[roster['Unnamed: 4'] == 'Pitcher'][['Name', 'Ht', 'Wt']]
+    pitchers = team_pitchers if pitchers is None else pd.concat([pitchers, team_pitchers], ignore_index=True)
+    time.sleep(1)
+
+# Keep Name, Height, Weight
+print(pitchers)
+pitchers.to_csv("pitchers.csv")
