651d55fe82546850fc2a34592265a398197a8a71
Eddie Morelli <edmo2495@gmail.com>
2025-08-16 16:19:42 -0500

    experimenting with z-scores instead of percentile

--- fangraphs_pull.py
+++ fangraphs_pull.py
@@ -1,11 +1,13 @@
+# Pull all pitchers' stats from FanGraphs
+
 import pybaseball
 
+# only pulling stats that I think can be relative to HOSS score
 stat_columns = ["Name", "WAR", "G", "GS", "SO", "K/9", "FIP", "K%", "FB% 2", "vFA (sc)", "vSI (sc)", "vFC (sc)", "Zone%", "SwStr%", "Stuff+", "Pitching+"]
 
 pitching_stats = pybaseball.pitching_stats(
     start_season=2025,
     end_season=2025,
-    # stat_columns=stat_columns,
     qual = 30,
 )
 
